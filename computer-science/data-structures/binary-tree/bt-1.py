6ef18d69293c71185b94eac90d3bf68a10095c01
asari <32879431+deomorxsy@users.noreply.github.com>
2021-10-20 09:55:00 -0300

    adding simple binary-tree bt-1.py, v1

---
+++ computer-science/data-structures/binary-tree/bt-1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class binaryTree:
+    '''
+    def __init__(self, value):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+    '''
+
+    def arvore(self, value):
+num, left_child, right_child):
+
+
+def main():
+
+	print("\nMENU DE OPÇÕES - ÁRVORE BINÁRIA\n");
+	print("1 - Inserir número");
+	print("2 - Consultar números pares");
+	print("3 - Consultar toda a árvore em ordem");
+	print("4 - Esvaziar a árvore");
+	print("5 - Sair");
+	System.out.print("Digite sua opção: ");
+if __name__ == "__main__":
+    main()
